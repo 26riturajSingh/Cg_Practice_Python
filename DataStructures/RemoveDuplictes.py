38bf96114899b6c730275bf35e6e3ea47443e86f
26riturajSingh <rituraj.a.singh@capgemini.com>
2025-12-16 11:35:08 +0530

    [Rituraj] ADD . DS Q 1

---
+++ DataStructures/RemoveDuplictes.py
@@ -0,0 +1,9 @@
+from importlib.resources.readers import remove_duplicates
+
+list1 = ["Hello", "World", "How", "Hello", 2, 6, 2, 77, 9, 77, "World"]
+
+print(list1)
+
+set1 = set(list1)
+
+print(set1)
